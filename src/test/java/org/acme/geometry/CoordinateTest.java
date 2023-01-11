ca07552a58cd296980d7880332d1004082c86847
GT200 <tiazaragaetan@gmail.com>
2023-01-11 16:43:10 +0100

    Coordinate (2D)

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -10,9 +10,17 @@ public class CoordinateTest {
 	@Test
 	public void testDefaultConstructor(){
 		// TODO
-		//Coordinate c = new Coordinate();
-		//Assert.assertEquals(0.0, c.getX(), EPSILON);
-		//Assert.assertEquals(0.0, c.getY(), EPSILON);
+		Coordinate c = new Coordinate();
+		Assert.assertEquals(0.0, c.getX(), EPSILON);
+		Assert.assertEquals(0.0, c.getY(), EPSILON);
+	}
+
+	@Test
+	public void testConstructorWithCoordinate(){
+		// TODO
+		Coordinate c = new Coordinate(2.0, 2.2);
+		Assert.assertEquals(2.0, c.getX(), EPSILON);
+		Assert.assertEquals(2.2, c.getY(), EPSILON);
 	}
 
 }
