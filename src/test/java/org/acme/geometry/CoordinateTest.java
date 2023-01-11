93b556c024cc08c8f74948df311aad457f8a362c
GT200 <tiazaragaetan@gmail.com>
2023-01-11 17:09:12 +0100

    Geometry, Point with test

--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -9,7 +9,7 @@ public class CoordinateTest {
 
 	@Test
 	public void testDefaultConstructor(){
-		// TODO
+
 		Coordinate c = new Coordinate();
 		Assert.assertEquals(0.0, c.getX(), EPSILON);
 		Assert.assertEquals(0.0, c.getY(), EPSILON);
@@ -17,7 +17,7 @@ public class CoordinateTest {
 
 	@Test
 	public void testConstructorWithCoordinate(){
-		// TODO
+
 		Coordinate c = new Coordinate(2.0, 2.2);
 		Assert.assertEquals(2.0, c.getX(), EPSILON);
 		Assert.assertEquals(2.2, c.getY(), EPSILON);
---
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -0,0 +1,41 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    public static final double EPSILON = 1.0e-15;
+
+    @Test
+    public void testDefaultConstructor(){
+
+        Point p = new Point();
+        Assert.assertEquals(0.0,p.getCoordinate().getX(), EPSILON);
+        Assert.assertEquals(0.0, p.getCoordinate().getY(), EPSILON);
+    }
+
+    @Test
+    public void testConstructorWithCoordinate(){
+        Coordinate c = new Coordinate(2.0, 2.2);
+        Point p = new Point(c);
+        Assert.assertEquals(2.0,p.getCoordinate().getX(), EPSILON);
+        Assert.assertEquals(2.2, p.getCoordinate().getY(), EPSILON);
+    }
+
+    @Test
+    public void testGetType(){
+        Coordinate c = new Coordinate(2.0, 2.2);
+        Point p = new Point(c);
+        Assert.assertEquals("POINT",p.getType());
+    }
+
+    @Test
+    public void testGetCoordinate(){
+        Coordinate c = new Coordinate(2.0, 2.2);
+        Point p = new Point(c);
+        Assert.assertEquals(c,p.getCoordinate());
+    }
+
+
+}
